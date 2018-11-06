39ec0d74db3d33939d0bdc0c86dbbb8436878312
Nathan Bell <nathanbell09@gmail.com>
2018-11-06 14:40:17 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(start_num)
+  count = start_num
+  while count > 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
